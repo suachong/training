496a931b7c334a86f947d6973946bbcc045ce09c
Su Ann Chong <suachong@amd.com>
2025-08-13 17:23:42 -0500

    use standardized path for mlperf_llama31_8b dataset and model

--- small_llm_pretraining/nemo/dev/run_docker.sh
+++ small_llm_pretraining/nemo/dev/run_docker.sh
@@ -6,8 +6,8 @@ docker run -it --rm \
     --net=host --uts=host \
     --ipc=host --device /dev/dri --device /dev/kfd \
     --security-opt=seccomp=unconfined \
-    --volume=/data2/mlperf_llama31_8b/data:/data \
-    --volume=/data2/mlperf_llama31_8b/model:/model \
+    --volume=/data/mlperf_llama31_8b/data:/data \
+    --volume=/data/mlperf_llama31_8b/model:/model \
+    --volume=/data/mlperf_llama31_8b/outputs:/outputs \
     --volume $(pwd):/workspace/code/ \
-    --volume=/data2/mlperf_llama31_8b/outputs:/outputs \
     --name smal-llm-training-`whoami` rocm/mlperf:llama31_8b_training_5.1_gfx942_v2
